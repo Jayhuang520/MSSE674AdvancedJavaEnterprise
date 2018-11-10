4f4a469b00fb03d4f726e1de78953355f5da1674
JayHuang <kingsleyzhijiehuang@gmail.com>
2018-11-09 18:41:43 -0700

    Modify the loginCroller java class.

--- src/com/huangsreservationsystem/controller/servlet/login/LoginController.java
+++ src/com/huangsreservationsystem/controller/servlet/login/LoginController.java
@@ -4,32 +4,50 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/LoginController")
 public class LoginController extends HttpServlet {
-       
-    /**
+
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 8095282046652575492L;
 
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
-		doPost(req,res);
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginController() {
+		super();
 	}
-	
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		
-		res.setContentType("text/html");
-		PrintWriter out = res.getWriter();
-		out.print("<html>");
-		out.print("<head><title>Acme login</title></head>");
-		out.print("<body>");
+		//get the input parameters
 		
-		out.print("login Controller");
+		//process the data
 		
-		out.print("</body>");
-		out.print("</html>");
+		//generate a response
+		
+		doPost(req, res);
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+//		res.setContentType("text/html");
+//		PrintWriter out = res.getWriter();
+//		out.print("<html>");
+//		out.print("<head><title>Acme login</title></head>");
+//		out.print("<body>");
+//
+//		out.print("login Controller");
+//
+//		out.print("</body>");
+//		out.print("</html>");
+		
+		
 	}
 }
